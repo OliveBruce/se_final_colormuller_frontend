bf47a8fc16caa3fa66e9028c73df58e7590d1b48
Olivia Bruce <olivia.bruce1995@gmail.com>
2025-02-03 19:54:52 -0600

    Create project infrastructure

---
+++ color_muller/src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import colormullerLogo from "../../assets/colormuller.png";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={colormullerLogo} className="logo" alt="Vite logo" />
+      </div>
+    </>
+  );
+}
+
+export default App;

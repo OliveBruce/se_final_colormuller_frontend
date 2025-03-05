323671a70242936c975436a18cfe38e7961b068f
Olivia Bruce <olivia.bruce1995@gmail.com>
2025-03-05 09:25:20 -0600

    update vite.config and package.json

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/se_final_colormuller_frontend/",
   plugins: [react()],
+  base: "/se_final_colormuller_frontend/",
   server: {
     port: 3000,
   },

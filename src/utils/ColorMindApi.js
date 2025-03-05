89a5889466a2d7302eb204245815253eff0619d6
Olivia Bruce <olivia.bruce1995@gmail.com>
2025-03-05 09:46:03 -0600

    change colormind.api request site

--- src/utils/ColorMindApi.js
+++ src/utils/ColorMindApi.js
@@ -2,10 +2,13 @@ import { request } from "./api.js";
 import { rgbToHex } from "./color.js";
 
 export const getRandomPalette = async () => {
-  const response = await fetch("https://colormind.io/api/", {
-    method: "POST",
-    body: JSON.stringify({ model: "default" }),
-  });
+  const response = await fetch(
+    "https://www.google.com/search?q=%http://colormind.io/api/&btnI=Im+Feeling+Lucky",
+    {
+      method: "POST",
+      body: JSON.stringify({ model: "default" }),
+    }
+  );
   if (!response.ok) {
     throw new Error("Failed to fetch palette");
   }
